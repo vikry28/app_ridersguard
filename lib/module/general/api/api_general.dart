ea47fb57657be04739e805d94e654b1e4780900f
vikry28 <vikryt@gmail.com>
2025-07-20 17:00:46 +0700

    Reset main to empty

--- lib/module/general/api/api_general.dart
+++
@@ -1,7 +0,0 @@
-import 'package:app_riderguard/core/networks/api_base.dart';
-
-class ApiGeneral extends ApiBase {
-  Future<Map> generalInfo() async {
-    return await get('/general/info', withAuth: false);
-  }
-}
